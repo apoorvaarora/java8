ed7852f07e85475d8e8450c17ebadb59d696aeed
Apoorva Arora <apoorva.090@gmail.com>
2021-11-14 19:37:00 +0530

    Create DaemonThreadDemo.java

---
+++ com/mslc/training/daemonthreads/DaemonThreadDemo.java
@@ -0,0 +1,14 @@
+package com.mslc.training.daemonthreads;
+
+import java.util.Timer;
+
+public class DaemonThreadDemo {
+
+	public static void main(String[] args) {
+
+		Timer timer = new Timer(false);
+
+		System.out.println("timer created...");
+
+	}
+}
